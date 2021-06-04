07b8dac8de1df90a2895ed6b4d4e362bcef361b3
Angel811-profesional <84739751+Angel811-profesional@users.noreply.github.com>
2021-06-04 12:12:57 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var usuario = "angel.gaytan811@gmail.com";
+var txt = "12345a";
+var u1;
+var i2;
+function IniciarSecion(){
+    location.href = "https://www.youtube.com/watch?v=J04uNivXGHs"
+     u1 == document.getElementById("iniciar").value;
+     i2 == document.getElementById("pass").value;
+
+     if(u1 == usuario && i2 == txt){
+      location.href = "https://www.youtube.com/watch?v=J04uNivXGHs";   
+     }else{
+         document.write("Upps algo ah salido mal...")
+     }
+}
